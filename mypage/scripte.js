5c650d506d7916263f230d0a525c04c42a565093
나범수 <123361606+pass98@users.noreply.github.com>
2023-07-31 19:50:35 +0900

    탭메뉴 간격
    탭메뉴 간격 margin top 50px

--- mypage/scripte.js
+++ mypage/scripte.js
@@ -28,3 +28,17 @@ $(document).ready(function(){
     //   }
     // }
   
+
+    $(document).ready(function(){
+  
+      $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+    
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+    
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+      })
+    
+    })
