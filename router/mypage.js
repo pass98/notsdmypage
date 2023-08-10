42c38f7ee344fb0f303158d440a353ee49328515
나범수 <123361606+pass98@users.noreply.github.com>
2023-08-10 12:08:04 +0900

    마이페이지 수정
    마이페이지 수정

--- router/mypage.js
+++ router/mypage.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 
 
 // 사용자 프로필 페이지
-router.get("/myapge", (req, res) => {
+router.get("/mypage", (req, res) => {
       // res.send(`<h1>Hello, ${req.user.displayName}</h1>`);
       res.render("myPage.html");
   });
